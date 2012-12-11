4a9d4fe045b9a4450ea5de4308ba38777652639c
Alexander Dymo <alex@alexdymo.com>
2012-12-11 17:09:22 +0200

    Make it possible to use image_tag with small images that do not require thumbnail generation

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -38,28 +38,41 @@ module Jekyll
             image_path = File.join(File.expand_path(File.dirname(__FILE__)), "..", @filename)
             width, height = FastImage.size(image_path)
             ratio = width.to_f / height
-            # pick nearest multiple of LINE_HEIGHT as thumbnail height
-            # to maintain the text vertical rhytm
-            thumbnail_height = (THUMBNAIL_MAX_WIDTH / ratio / LINE_HEIGHT.to_f).floor * LINE_HEIGHT
 
-            # TODO: skip
-            # autogenerate thumbnail if it doesn't exist yet
-            # we need thumbnails because default
-            thumbnail_filename = @filename.gsub(".png", "_thumbnail.png")
-            thumbnail_path = File.join(File.expand_path(File.dirname(__FILE__)), "..", thumbnail_filename)
-            unless File.exists?(thumbnail_path)
-                # Note: requires convert (from ImageMagick) and pngnq installed
-                `convert -resize x#{thumbnail_height} "#{image_path}" "#{thumbnail_path}"`
-                `pngnq -e ".png" -f "#{thumbnail_path}"`
+            if width > THUMBNAIL_MAX_WIDTH
+                # need thumbnail for the image
+                # pick nearest multiple of LINE_HEIGHT as thumbnail height
+                # to maintain the text vertical rhytm
+                thumbnail_height = (THUMBNAIL_MAX_WIDTH / ratio / LINE_HEIGHT.to_f).floor * LINE_HEIGHT
+
+                # autogenerate thumbnail if it doesn't exist yet
+                # we need thumbnails because default
+                thumbnail_filename = @filename.gsub(".png", "_thumbnail.png")
+                thumbnail_path = File.join(File.expand_path(File.dirname(__FILE__)), "..", thumbnail_filename)
+                unless File.exists?(thumbnail_path)
+                    # Note: requires convert (from ImageMagick) and pngnq installed
+                    `convert -resize x#{thumbnail_height} "#{image_path}" "#{thumbnail_path}"`
+                    `pngnq -e ".png" -f "#{thumbnail_path}"`
+                end
+                thumbnail_width, thumbnail_height = FastImage.size(thumbnail_path)
+
+                <<-EOS
+                <div class="figure" style="width: #{thumbnail_width}px;">
+                    <a href="#{@filename}"><img src="#{thumbnail_filename}" height="#{thumbnail_height}" width="#{thumbnail_width}" /></a>
+                    <div class="figcaption">#{@caption}</div>
+                </div>
+                EOS
+            else
+                # no thumbnail required, just display image as is
+                # note: image height must already be a multiple of LINE_HEIGHT
+                # no automatic conversion is done in this case
+                <<-EOS
+                <div class="figure" style="width: #{width}px;">
+                    <img src="#{@filename}" height="#{height}" width="#{width}" /></a>
+                    <div class="figcaption">#{@caption}</div>
+                </div>
+                EOS
             end
-            thumbnail_width, thumbnail_height = FastImage.size(thumbnail_path)
-
-            <<-EOS
-            <div class="figure" style="width: #{thumbnail_width}px;">
-                <a href="#{@filename}"><img src="#{thumbnail_filename}" height="#{thumbnail_height}" width="#{thumbnail_width}" /></a>
-                <div class="figcaption">#{@caption}</div>
-            </div>
-            EOS
         end
     end
 end
