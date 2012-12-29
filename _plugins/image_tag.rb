886f91735b0cba3a4b43f96ad06011794b1740af
Alexander Dymo <alex@alexdymo.com>
2012-12-29 11:33:42 +0200

    Move Jekyll and Liquid extensions to their own file

--- _plugins/image_tag.rb
+++ _plugins/image_tag.rb
@@ -1,24 +1,6 @@
 require 'rubygems'
 require 'fastimage'
 
-# Hack to raise liquid errors from jekyll.
-# By default it just eats them, see
-# https://github.com/mojombo/jekyll/pull/624
-class Liquid::Template
-    def initialize
-        @rethrow_errors = true;
-    end
-end
-# Jekyll was not designed to handle liquid exceptions in posts,
-# it expects Post::name function in order to display the exception
-class Jekyll::Post
-    def name
-        @name
-    end
-end
-
-
-
 module Jekyll
 
     class ImageTag < Liquid::Tag
